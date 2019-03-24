7780dc561ffab7625ba407b1213811a76dbfc721
alydubel <alexander@hackl.com>
2019-03-24 20:53:24 +0100

    Add user information

--- webfrontend/htmlauth/index.php
+++ webfrontend/htmlauth/index.php
@@ -1,8 +1,56 @@
 <html>
  <head>
-  <title>PHP-Test</title>
+  <title>InfNodGra</title>
  </head>
  <body>
- <?php echo '<p>Hallo Welt</p>'; ?>
+ <?php echo '<p>Docker-Plugin: InfluxDB /  Node-Red / Grafana</p>'; ?>
+	<br/>
+	<br/>
+	Experimente Installation der Docker Container zum testen und ausprobieren.
+
+	<br/>
+	<br/>
+	InfluxDB läuft auf Port 8086 
+	<br/>
+	Benutzername: dbadmin
+	<br/>
+	Passwort: dbadmin
+	<br/>
+	<br/>
+	Um eine Datenbank zu erstellen kann folgende Anweisung benutzt werden:
+	<br/>
+	<code>curl -i -XPOST "http://xxx.xxx.xxx.xxx:8086/query?u=dbadmin&p=dbadmin" --data-urlencode "q=CREATE DATABASE loxdb"</code>
+
+	<br/>
+	<br/>
+	Node-Red läuft auf Port 1880 
+	<br/>
+	ohne Zugangskennung
+	<br/>
+	<br/>
+	Um sich mit der Datenbank zu verbinden ist noch ein Plugin über das Menu "Palette verwalten" zu installieren
+	<br/>
+	node-red-contrib-influxdb
+	<br/>
+	node-red-dashboard
+	<br/>
+	<br/>
+	Optional:
+	<br/>
+	node-red-node-openweathermap
+	
+	
+	<br/>
+	<br/>
+	Grafana läuft auf Port 3000 
+	<br/>
+	Benutzername: admin
+	<br/>
+	Passwort: admin
+
+	
+
+	
+
  </body>
 </html>

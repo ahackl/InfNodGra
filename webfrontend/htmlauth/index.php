1cf756847a25c653afe504e41f9a1cee7354f41b
alydubel <alexander@hackl.com>
2019-03-24 21:13:26 +0100

    update plugin page

--- webfrontend/htmlauth/index.php
+++ webfrontend/htmlauth/index.php
@@ -1,56 +1,61 @@
-<html>
- <head>
-  <title>InfNodGra</title>
- </head>
- <body>
- <?php echo '<p>Docker-Plugin: InfluxDB /  Node-Red / Grafana</p>'; ?>
-	<br/>
-	<br/>
-	Experimentelle Installation der Docker Container zum testen und ausprobieren.
+<?php
+require_once "loxberry_web.php";
+  
+// This will read your language files to the array $L
+$template_title = "InfNodGra";
+$helplink = "nopanels";
+$helptemplate = "";
+  
+LBWeb::lbheader($template_title, $helplink, $helptemplate);
+ 
+// This is the main area for your plugin
+?>
 
-	<br/>
-	<br/>
-	InfluxDB läuft auf Port 8086 
-	<br/>
-	Benutzername: dbadmin
-	<br/>
-	Passwort: dbadmin
-	<br/>
-	<br/>
-	Um eine Datenbank zu erstellen kann folgende Anweisung benutzt werden:
-	<br/>
-	<code>curl -i -XPOST "http://xxx.xxx.xxx.xxx:8086/query?u=dbadmin&p=dbadmin" --data-urlencode "q=CREATE DATABASE loxdb"</code>
+Experimentelle Installation der Docker Container zum testen und ausprobieren.
 
-	<br/>
-	<br/>
-	Node-Red läuft auf Port 1880 
-	<br/>
-	ohne Zugangskennung
-	<br/>
-	<br/>
-	Um sich mit der Datenbank zu verbinden ist noch ein Plugin über das Menu "Palette verwalten" zu installieren
-	<br/>
-	node-red-contrib-influxdb
-	<br/>
-	node-red-dashboard
-	<br/>
-	<br/>
-	Optional:
-	<br/>
-	node-red-node-openweathermap
-	
-	
-	<br/>
-	<br/>
-	Grafana läuft auf Port 3000 
-	<br/>
-	Benutzername: admin
-	<br/>
-	Passwort: admin
+<br/>
+<br/>
+InfluxDB läuft auf Port 8086 
+<br/>
+Benutzername: dbadmin
+<br/>
+Passwort: dbadmin
+<br/>
+<br/>
+Um eine Datenbank zu erstellen kann folgende Anweisung benutzt werden:
+<br/>
+<code>curl -i -XPOST "http://xxx.xxx.xxx.xxx:8086/query?u=dbadmin&p=dbadmin" --data-urlencode "q=CREATE DATABASE loxdb"</code>
 
-	
+<br/>
+<br/>
+Node-Red läuft auf Port 1880 
+<br/>
+ohne Zugangskennung
+<br/>
+<br/>
+Um sich mit der Datenbank zu verbinden ist noch ein Plugin über das Menu "Palette verwalten" zu installieren
+<br/>
+node-red-contrib-influxdb
+<br/>
+node-red-dashboard
+<br/>
+<br/>
+Optional:
+<br/>
+node-red-node-openweathermap
 
-	
 
- </body>
-</html>
+<br/>
+<br/>
+Grafana läuft auf Port 3000 
+<br/>
+Benutzername: admin
+<br/>
+Passwort: admin
+
+
+ 
+<?php 
+// Finally print the footer 
+LBWeb::lbfooter();
+?>
